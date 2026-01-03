11b9eda4eefb8861d9f734df63abdd00df172560
krzysztof_binsoft <krzysztof@binsoft.pl>
2026-01-03 18:27:17 +0100

    FEAT: freezed package - simple usecase

--- lib/features/zoo/user.dart
+++ lib/features/zoo/user.dart
@@ -1,49 +1,29 @@
-// import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:organizer/dev/annotations.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-// part 'user.freezed.dart';
-// part 'user.g.dart';
-part 'user.json.dart';
-part 'user.copy_with.dart';
-// @freezed
-// abstract class User with _$User {
-//   const factory User({
-//     required String firstName,
-//     required String lastName,
-//     required String email,
-//     required String password,
-//   }) = _User;
+part 'user.freezed.dart';
+part 'user.g.dart';
 
-//   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
-// }
+@freezed
+abstract class User with _$User {
+  const factory User({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+  }) = _User;
 
-@MyJsonSerializable()
-@MyCopyWith()
-class User {
-  final String name;
-  final String lastName;
-  final int age;
-  final String email;
-  final String password;
-  User({
-    required this.name,
-    required this.lastName,
-    required this.age,
-    required this.email,
-    required this.password,
-  });
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 }
 
 void testUser() {
   final user = User(
-    name: 'John',
+    firstName: 'John',
     lastName: 'Doe',
     email: 'john.doe@example.com',
     password: 'password',
-    age: 30,
   );
   print(user.toJson());
-  final u2 = user.copyWith(name: 'Jan', lastName: 'Kowalski');
+  final u2 = user.copyWith(firstName: 'Jan', lastName: 'Kowalski');
   print(u2.toJson());
   // print(user == u2);
   // print(user.hashCode);
